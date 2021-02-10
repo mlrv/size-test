4f2ba1396beb3753011c8575de9165889ba0c4d9
marco <marco.levrero@gmail.com>
2021-02-10 09:26:59 +0000

    Test diff

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
-import { filterMap } from "fp-ts/Array"
+import { filterMap, makeBy } from "fp-ts/Array"
 import { some } from "fp-ts/Option"
 
 const a = filterMap(
   (x: number) => some(x+ 1)
 )([1, 2, 3])
 
+const b = makeBy(10, _ => 1)
+
 console.log(a)
+console.log(b)
